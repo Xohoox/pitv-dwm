516cc82577906dace1ef0e94ec6e6282d981a333
Fynn Michell-Auli <f.michell-auli@live.de>
2024-11-15 19:56:36 +0100

    patch: autostarttags

--- config.def.h
+++ config.def.h
@@ -64,6 +64,13 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
+static const char *browsercmd[] = { "brave-browser", NULL };
+// static const char *termcmd2[] = { "kodi", NULL }; TODO: install
+
+Autostarttag autostarttaglist[] = {
+	{.cmd = browsercmd, .tags = 2 },
+};
+
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
